f2cc396085f83bc3e26232e298f4173546dbfacf
sakutaku <saikalabytova1@gmail.com>
2023-10-04 22:07:21 +0600

    HW#91 - added googleId constant in the front and realized wrap in index for it

--- front/src/constants.ts
+++ front/src/constants.ts
@@ -4,3 +4,4 @@ export enum userRoles {
   admin = 'admin',
   user = 'user'
 }
+export const GOOGLE_CLIENT_ID = '403608350944-ru56h6iakhjkoehjbqtpvuah7lgvmj06.apps.googleusercontent.com';
--- front/src/index.tsx
+++ front/src/index.tsx
@@ -7,6 +7,8 @@ import { Provider } from 'react-redux';
 import { persistor, store } from './app/store';
 import { PersistGate } from 'redux-persist/integration/react';
 import { addInterceptors } from './axiosApi';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import { GOOGLE_CLIENT_ID } from './constants';
 
 addInterceptors(store);
 
@@ -14,12 +16,14 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <Provider store={store}>
-    <PersistGate persistor={persistor}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </PersistGate>
-  </Provider>
+  <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
+  </GoogleOAuthProvider>
 );
 
